cb9b782f1da63cae135959aa3b66f6c3286177f5
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-04-09 09:49:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SearchIcon from "./search.svg";
 import "./App.css";
 
 
-const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
+const API_URL = "https://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
